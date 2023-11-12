d139339ecb24e8e6e4c1455ac81a9a45f41716fa
Crowdbotics <team@crowdbotics.com>
2023-11-12 02:10:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-voice-44492",
+  url: "https://divine-voice-44492.botics.co",
+  api: "https://divine-voice-44492.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
